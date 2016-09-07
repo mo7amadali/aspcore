250d29f99e35bbb35f39b5047633c6f17daef5ee
Mohammed  Ali <mohamedali@aucegypt.edu>
2016-09-07 09:28:20 +0200

    =Adding bootstrap and font awesome

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -17,14 +17,18 @@
     //    alert($(this).text());
     //});
     var sidebarAndWrapper = $("#sidebar,#wrapper");
+    var icon = $("#sidebarToggle i.fa");
     $("#sidebarToggle").on("click", function ()
     {
         sidebarAndWrapper.toggleClass("hide-sidebar");
         if (sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
+            icon.addClass("fa-angle-right");
+            icon.removeClass("fa-angle-left");
+            
         }
         else {
-            $(this).text("Hide Sidebar");
+            icon.addClass("fa-angle-left");
+            icon.removeClass("fa-angle-right");
         }
     })
 })();
